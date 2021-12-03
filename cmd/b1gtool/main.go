41e420e9128f6648503d148130b5ef641077e47e
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-12-02 22:25:37 -0500

    Clean up

--- cmd/b1gtool/main.go
+++ cmd/b1gtool/main.go
@@ -1,55 +1,8 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"sort"
-	"strings"
-
 	"github.com/alecthomas/kong"
 )
-
-// ProjectID is the Google Cloud Project ID where the season data will be loaded.
-var ProjectID string
-
-// Force, if set, forcefully overwrite data in Firestore instead of failing if the documents already exist.
-var Force bool
-
-// DryRun, if true, will print the firestore objects to console rather than writing them to firestore.
-var DryRun bool
-
-func usage() {
-	cs := make([]string, len(Commands))
-	i := 0
-	for command := range Commands {
-		cs[i] = command
-		i++
-	}
-	sort.Strings(cs)
-	cstring := strings.Join(cs, "\n  ")
-	fmt.Fprintf(flag.CommandLine.Output(), `Usage: b1gtool [global-flags] <command>
-
-B1GTool: a command-line tool for managing B1G Pick 'Em data and picks.
-
-Commands:
-  %s
-
-Global Flags:
-`, cstring)
-
-	flag.PrintDefaults()
-}
-
-// Commands are nullary functions that are run when commands (the keys of the map) are given as the first argument to the program.
-var Commands map[string]func() = make(map[string]func())
-
-func init() {
-	flag.Usage = usage
-
-	flag.StringVar(&ProjectID, "project", "", "Google Cloud Project ID.  If equal to the empty string, the environment variable GCP_PROJECT will be used.")
-	flag.BoolVar(&Force, "force", false, "Force overwrite of data in Firestore with the SET command rather than failing if the data already exists.")
-	flag.BoolVar(&DryRun, "dryrun", false, "Do not write to firestore, but print to console instead.")
-}
 
 type globalCmd struct {
 	ProjectID string `help:"GCP project ID." env:"GCP_PROJECT" required:""`
